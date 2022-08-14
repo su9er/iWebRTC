49d9e2e14d5196b7a0204e48aa4bd19cecb3b93d
su9er <liangchaohong@163.com>
2022-08-14 23:07:46 +0800

    feat: 增加日志模块

--- packages/manage-server/src/app.module.ts
+++ packages/manage-server/src/app.module.ts
@@ -4,6 +4,8 @@ import { RoomModule } from '@modules/room/room.module';
 import { configGenerator } from '@config/index';
 import { HealthcheckModule } from './modules/healthcheck/healthcheck.module';
 import { RpcModule } from './modules/rpc/rpc.module';
+import { MiddlewareConsumer } from '@nestjs/common';
+import LogsMiddleware from '@middlewares/logs.middleware';
 
 @Module({
   imports: [
@@ -16,4 +18,8 @@ import { RpcModule } from './modules/rpc/rpc.module';
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogsMiddleware).forRoutes('*');
+  }
+}
